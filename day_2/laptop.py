aabd558300dd4e6614740a0b6ba33f7cb9cf3208
HS-HANITH <hanithsalian5@gmail.com>
2026-02-06 10:22:30 +0530

    Add files via upload
    Signed-off-by: HS-HANITH <hanithsalian5@gmail.com>

---
+++ day_2/laptop.py
@@ -0,0 +1,7 @@
+item_name = "laptop"
+quantity=2
+price =500
+in_stock = True
+print("item:",item_name,"quantity:",quantity,"price:",price,"available:",in_stock)
+total_cost = quantity * price
+print("Total cost:", total_cost)
---
+++ day_2/splitbill.py
@@ -0,0 +1,11 @@
+bill = float(input("Enter the total bill amount: "))
+people = int(input("Enter the number of people: "))
+
+share = bill / people
+
+
+print(f"Each person pays: {share}")
+
+print(type(bill))
+print(type(people))
+print(type(share))
