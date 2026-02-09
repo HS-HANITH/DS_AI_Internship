c5c2d038b278029ac06e291abda602c1827fad3d
HS-HANITH <hanithsalian5@gmail.com>
2026-02-09 12:21:26 +0530

    Add files via upload
    Signed-off-by: HS-HANITH <hanithsalian5@gmail.com>

---
+++ day_6/journal.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+goal = input("Enter your daily goal: ")
+
+with open("journal.txt", "a") as file:
+    file.write(f"{name} - {goal}\n")
+
+print("Saved successfully!")
+
